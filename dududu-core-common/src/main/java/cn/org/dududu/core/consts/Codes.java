cd1b326076862b808226fd4c1be74383e7affb38
echo-zheng <18594283@qq.com>
2019-11-01 10:33:41 +0800

    【新增】项目初始化

--- dududu-core-common/src/main/java/cn/org/dududu/core/consts/Codes.java
+++ dududu-core-common/src/main/java/cn/org/dududu/core/consts/Codes.java
@@ -8,10 +8,13 @@ import cn.org.dududu.core.annotation.Const;
 public interface Codes {
     @Const("成功")
     int SUCCESS = 0;
+
     @Const("失败")
     int FAILED = 1;
+
     @Const("未授权")
     int UN_AUTHORIZE = 2;
+
     @Const("非法访问")
     int ILLEGAL_ACCESS = 3;
 }
